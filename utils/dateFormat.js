159ecf985a25ee2adf8b4bf453e85d93cdfc91ad
Your Name <daquan.jordan22@gmail.com>
2022-06-12 20:38:47 -0400

    dateformat remastered

--- utils/dateFormat.js
+++ utils/dateFormat.js
@@ -1,98 +1,98 @@
-const addDateSuffix = date => {
-    let dateStr = date.toString();
-  
-    // get last char of date string
-    const lastChar = dateStr.charAt(dateStr.length - 1);
-  
-    if (lastChar === '1' && dateStr !== '11') {
-      dateStr = `${dateStr}st`;
-    } else if (lastChar === '2' && dateStr !== '12') {
-      dateStr = `${dateStr}nd`;
-    } else if (lastChar === '3' && dateStr !== '13') {
-      dateStr = `${dateStr}rd`;
+const showCorrectDate = date => {
+    let showDate = date.toString();
+
+ 
+    const endCharacter = showDate.charAt(showDate.length - 1);
+
+    if (endCharacter === '1' && showDate !== '11') {
+        showDate = `${showDate}st`;
+    } else if (endCharacter === '2' && showDate !== '12') {
+        showDate = `${showDate}nd`;
+    } else if (endCharacter === '3' && showDate !== '13') {
+        showDate = `${showDate}rd`;
     } else {
-      dateStr = `${dateStr}th`;
+        showDate = `${showDate}th`;
     }
-  
-    return dateStr;
-  };
-  
-  // function to format a timestamp, accepts the timestamp and an `options` object as optional parameters
-  module.exports = (
+
+    return showDate;
+};
+
+
+module.exports = (
     timestamp,
-    { monthLength = 'short', dateSuffix = true } = {}
-  ) => {
+    { monthLength = 'short', dateTime = true } = {}
+) => {
     let months;
-  
+
     if (monthLength === 'short') {
-      months = {
-        0: 'Jan',
-        1: 'Feb',
-        2: 'Mar',
-        3: 'Apr',
-        4: 'May',
-        5: 'Jun',
-        6: 'Jul',
-        7: 'Aug',
-        8: 'Sep',
-        9: 'Oct',
-        10: 'Nov',
-        11: 'Dec'
-      };
+        months = {
+            0: 'Jan',
+            1: 'Feb',
+            2: 'Mar',
+            3: 'Apr',
+            4: 'May',
+            5: 'Jun',
+            6: 'Jul',
+            7: 'Aug',
+            8: 'Sep',
+            9: 'Oct',
+            10: 'Nov',
+            11: 'Dec'
+        };
     } else {
-      months = {
-        0: 'January',
-        1: 'February',
-        2: 'March',
-        3: 'April',
-        4: 'May',
-        5: 'June',
-        6: 'July',
-        7: 'August',
-        8: 'September',
-        9: 'October',
-        10: 'November',
-        11: 'December'
-      };
+        months = {
+            0: 'January',
+            1: 'February',
+            2: 'March',
+            3: 'April',
+            4: 'May',
+            5: 'June',
+            6: 'July',
+            7: 'August',
+            8: 'September',
+            9: 'October',
+            10: 'November',
+            11: 'December'
+        };
     }
-  
-    const dateObj = new Date(timestamp);
-    const formattedMonth = months[dateObj.getMonth()];
-  
+
+    const dateObject = new Date(timestamp);
+    const timeOfMonth = months[dateObject.getMonth()];
+
     let dayOfMonth;
-  
-    if (dateSuffix) {
-      dayOfMonth = addDateSuffix(dateObj.getDate());
+
+    if (dateTime) {
+        dayOfMonth = showCorrectDate(dateObject.getDate());
     } else {
-      dayOfMonth = dateObj.getDate();
+        dayOfMonth = dateObject.getDate();
     }
-  
-    const year = dateObj.getFullYear();
-  
+
+    const year = dateObject.getFullYear();
+
     let hour;
-    // check for 24-hr time
-    if (dateObj.getHours > 12) {
-      hour = Math.floor(dateObj.getHours() / 2);
+ 
+    if (dateObject.getHours > 12) {
+        hour = Math.floor(dateObject.getHours() / 2);
     } else {
-      hour = dateObj.getHours();
+        hour = dateObject.getHours();
     }
-    // if hour is 0 (12:00am), change it to 12
+  
     if (hour === 0) {
-      hour = 12;
+        hour = 12;
     }
+
+    const minutes = dateObject.getMinutes();
+
   
-    const minutes = dateObj.getMinutes();
-  
-    // set `am` or `pm`
-    let periodOfDay;
-  
-    if (dateObj.getHours() >= 12) {
-      periodOfDay = 'pm';
+    let timeOfDay;
+
+    if (dateObject.getHours() >= 12) {
+        timeOfDay = 'pm';
     } else {
-      periodOfDay = 'am';
+        timeOfDay = 'am';
     }
-  
-    const formattedTimeStamp = `${formattedMonth} ${dayOfMonth}, ${year} at ${hour}:${minutes} ${periodOfDay}`;
-  
-    return formattedTimeStamp;
-  };
+
+    const currentTimeStamp = `${timeOfMonth} ${dayOfMonth}, ${year} at ${hour}:${minutes} ${timeOfDay}`;
+
+    return currentTimeStamp;
+};
